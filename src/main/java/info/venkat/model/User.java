26dd51a42c9f8cda9c80a53063fec91cf85f92d3
vseshadri <vseshadri@manh.com>
2019-08-11 20:32:05 +0530

    spring datasource

--- src/main/java/info/venkat/model/User.java
+++ src/main/java/info/venkat/model/User.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Component;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Component
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String userId;
     @Column(nullable = false)
     private String userName;
