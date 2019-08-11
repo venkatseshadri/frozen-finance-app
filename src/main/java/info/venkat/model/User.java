2589e19f869a052c06cc27ad7fef582c38eff194
vseshadri <vseshadri@manh.com>
2019-08-11 11:26:44 +0530

    component annotation - annotation fix

--- src/main/java/info/venkat/model/User.java
+++ src/main/java/info/venkat/model/User.java
@@ -1,9 +1,12 @@
 package info.venkat.model;
 
+import org.springframework.stereotype.Component;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Component
 public class User {
     @Id
     private String userId;
--- src/main/java/info/venkat/service/impl/UserServiceImpl.java
+++ src/main/java/info/venkat/service/impl/UserServiceImpl.java
@@ -4,11 +4,13 @@ import info.venkat.model.User;
 import info.venkat.repository.UserRepository;
 import info.venkat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
