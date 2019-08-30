ff011265cded33829bc7595d6c0382b954108c8b
vseshadri <vseshadri@manh.com>
2019-08-30 14:57:46 +0530

    Dashboard Id generation fix

--- src/main/java/info/venkat/model/DashboardEntity.java
+++ src/main/java/info/venkat/model/DashboardEntity.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 
@@ -12,6 +14,8 @@ import java.util.Date;
 public class DashboardEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", unique=true, nullable=false)
     Long pk;
 
     @Column
